2e854514676785758a0dddac2624bfda1c335b1b
bryan <briplomo@gmail.com>
2024-06-27 00:04:43 -0500

    disable stop script

--- deployment-scripts/stop_server.sh
+++ deployment-scripts/stop_server.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/sh
 
-sudo kill -9 'cat api-gateway_pid.txt' || true
-sudo rm api-gateway_pid.txt || true
+#sudo kill -9 'cat api-gateway_pid.txt' || true
+#sudo rm api-gateway_pid.txt || true
 
