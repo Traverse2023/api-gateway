3653b594910b588a8db5aeccc143adabb2663c97
bryan <briplomo@gmail.com>
2024-07-01 11:14:48 -0500

    change path to run jar

--- deployment-scripts/start_server.sh
+++ deployment-scripts/start_server.sh
@@ -1,4 +1,5 @@
 #! /usr/bin/sh
 
-sudo nohup java -jar /home/server/api-gateway-0.0.1.jar >api_gateway.log 2>&1 &
+
+sudo nohup java -jar /home/ec2-user/server/api-gateway-0.0.1.jar >api_gateway.log 2>&1 &
 echo $! > api_gateway_pid.txt
