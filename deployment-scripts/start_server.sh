6d593a11f0f7cfa905eeb9ad8df2a22ffccae31e
bryan <briplomo@gmail.com>
2024-07-01 19:07:05 -0500

    source envs in start script

--- deployment-scripts/start_server.sh
+++ deployment-scripts/start_server.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/sh
 
-
-sudo nohup java -jar /home/ec2-user/server/api-gateway-0.0.1.jar >api_gateway.log 2>&1 &
+source /etc/profile
+sudo nohup java -jar /home/ec2-user/server/api-gateway-0.0.1.jar > api_gateway.log 2>&1 &
 echo $! > api_gateway_pid.txt
