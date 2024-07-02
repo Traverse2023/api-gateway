55e6c0c47b730bf99faaf20fafc9031326dad6a1
bryan <briplomo@gmail.com>
2024-07-02 00:11:22 -0500

    enable stop server to fix redeployment

--- deployment-scripts/start_server.sh
+++ deployment-scripts/start_server.sh
@@ -1,3 +1,8 @@
-#! /usr/bin/sh
+#!/bin/sh
 
-sudo nohup java -jar /home/ec2-user/server/api-gateway-0.0.1.jar > api_gateway.log 2>&1 &
+source /etc/profile
+sudo touch /api_gateway.log
+sudo chmod 777 /api_gateway.log
+nohup java -jar /home/ec2-user/server/api-gateway-0.0.1.jar >> api_gateway.log 2>&1 &
+echo $! > api_gateway_pid.txt
+
